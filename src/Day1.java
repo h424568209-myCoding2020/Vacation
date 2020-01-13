ed2dbd1f6c2ecd1bd40e2c9a939002d7647ee81b
h424568209 <424568209@qq.com>
2020-01-13 14:45:30 +0800

    假期练习

--- src/Day1.java
+++ src/Day1.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Day1 {
     public void mystery(int x){
         System.out.println(x%10);
@@ -20,6 +22,26 @@ public class Day1 {
         }
         return sb.toString();
     }
+    public void  rotate(int []nums,int k ){
+        int n = nums.length;
+        k%=n;
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
+        System.out.println(Arrays.toString(nums));
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while(start<end){
+            int temp = nums[start];
+            nums[start] = nums[end  ];
+            nums[end]=temp;
+            start++;
+            end--;
+        }
+
+    }
+
     public static void main(String[] args) {
         int a = 20,b = 5;
         //25255
@@ -28,5 +50,6 @@ public class Day1 {
         d.mystery(1234);
         String s = "DDCDAAb";
         System.out.println(d.toLowerString(s));
+        d.rotate(new int[]{1,2,3,4,5},3);
     }
 }
